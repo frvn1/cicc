e724a8665a4bb3223c70f88c451cbdad6af1a201
Fran <124851154+frvn1@users.noreply.github.com>
2024-02-16 01:23:36 -0300

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,83 @@
+  // jQuery(document).ready(function () {
+  //   $('#btnInforme').click(function () {
+  //     html2canvas(document.querySelector('body')).then((canvas) => {
+  //       let base64image = canvas.toDataURL('image/png');
+  //       // console.log(base64image);
+  //       let pdf = new jsPDF('p', 'px', [1600, 1131]);
+  //       pdf.addImage(base64image, 'PNG', 15, 15, 1140, 360);
+  //       pdf.save('Informe-medico-IMC.pdf');
+  //     });
+  //   });
+  // });
+$('#btnInforme').click(function (){
+  window.print();
+  $(window).print();
+}
+);
+
+var age = document.getElementById("age");
+var height = document.getElementById("height");
+var weight = document.getElementById("weight");
+var male = document.getElementById("m");
+var female = document.getElementById("f");
+var form = document.getElementById("form");
+let resultArea = document.querySelector(".comment");
+
+modalContent = document.querySelector(".modal-content");
+modalText = document.querySelector("#modalText");
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+
+
+function calculate(){
+ 
+  if(age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)){
+    modal.style.display = "block";
+    modalText.innerHTML = `Debes Completar TODOS los Campos!`;
+
+  }else{
+    countBmi();
+  }
+
+}
+
+
+function countBmi(){
+  var p = [age.value, height.value, weight.value];
+  if(male.checked){
+    p.push("male");
+  }else if(female.checked){
+    p.push("female");
+  }
+
+  var bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
+      
+  var result = '';
+  if(bmi<18.5){
+    result = 'Bajo Peso';
+     }else if(18.5<=bmi&&bmi<=24.9){
+    result = 'Saludable';
+     }else if(25<=bmi&&bmi<=29.9){
+    result = 'Sobre Peso';
+     }else if(30<=bmi&&bmi<=34.9){
+    result = 'Obesidad';
+     }else if(35<=bmi){
+    result = 'Mucha Obesidad';
+     }
+
+resultArea.style.display = "block";
+document.querySelector(".comment").innerHTML = `Estado: <span id="comment">${result}</span>`;
+document.querySelector("#result").innerHTML = bmi.toFixed(2);
+
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
